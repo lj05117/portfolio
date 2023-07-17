1629fa98897d02f670464c483d5b9a64dcc998a0
lj05117 <lj05117@naver.com>
2023-07-17 16:56:01 +0900

    필수 이미지 컴포넌트 생성

---
+++ src/components/RMImage/index.tsx
@@ -0,0 +1,9 @@
+import { RMImageStyle } from "./styles";
+
+import EssentialImage from "../../assets/FrontEnd-Test-Image.png";
+
+const RMImage = () => {
+  return <RMImageStyle src={EssentialImage}></RMImageStyle>;
+};
+
+export default RMImage;
