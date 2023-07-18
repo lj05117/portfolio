af035bed0b415f0b2a97d9d6f58e73de8b9ffde8
lj05117 <lj05117@naver.com>
2023-07-18 09:16:24 +0900

    헤더 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import { RMImagePage } from "./pages/index";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<RMImagePage></RMImagePage>}></Route>
+        <Route path="/image" element={<RMImagePage></RMImagePage>}></Route>
       </Routes>
     </BrowserRouter>
   );
