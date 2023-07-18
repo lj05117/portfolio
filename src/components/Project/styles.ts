2f82ed8344c9116d64118dd26a502c3b496396c1
lj05117 <lj05117@naver.com>
2023-07-18 19:55:02 +0900

    프로젝트 컴포넌트 디자인

---
+++ src/components/Project/styles.ts
@@ -0,0 +1,28 @@
+import { styled } from "styled-components";
+
+export const ProjectContainer = styled.div`
+  margin: 200px 130px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ProjectImageStyle = styled.img`
+  height: 350px;
+`;
+
+export const ProjectContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 15px 200px 50px 50px;
+  text-align: center;
+`;
+
+export const ProjectLogo = styled.img`
+  height: 150px;
+`;
+
+export const ProjectTitleStyle = styled.h1``;
+
+export const ProjectContent = styled.div`
+  margin: 10px;
+`;
