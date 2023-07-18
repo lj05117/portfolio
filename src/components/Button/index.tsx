7c53768c687482ed0de4a253166c8931c3031b77
lj05117 <lj05117@naver.com>
2023-07-18 19:54:37 +0900

    버튼 링크 설정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,29 +2,39 @@ import { ButtonStyle } from "./styles";
 
 const Button = ({ buttonName }: { buttonName: string }) => {
   const goLink = (name: string) => {
-    name === "git"
-      ? (window.location.href = "https://github.com/lj05117")
-      : name === "velog"
-      ? (window.location.href = "https://velog.io/@lj05117")
-      : (window.location.href =
-          "https://shore-roadway-33f.notion.site/a05ef24e1eff4e0d93e7fff5889e124f");
+    if (name.slice(0, 3) === "git") {
+      if (name === "git") {
+        window.location.href = "https://github.com/lj05117";
+      } else if (name.slice(4) === "fyum") {
+        window.location.href = "https://github.com/docent-FY-UM/FYUM/tree/main";
+      } else if (name.slice(4) === "dawool") {
+        window.location.href = "https://github.com/muyeochu/dawool";
+      } else {
+        window.location.href = "https://github.com/SignBridge/signbridge";
+      }
+    } else if (name === "velog") {
+      window.location.href = "https://velog.io/@lj05117";
+    } else {
+      window.location.href =
+        "https://shore-roadway-33f.notion.site/a05ef24e1eff4e0d93e7fff5889e124f";
+    }
   };
 
-  if (buttonName === "git") {
+  if (buttonName.slice(0, 3) === "git") {
     return (
-      <ButtonStyle className={"git"} onClick={() => goLink("git")}>
+      <ButtonStyle className={"git"} onClick={() => goLink(buttonName)}>
         github
       </ButtonStyle>
     );
   } else if (buttonName === "velog") {
     return (
-      <ButtonStyle className={"velog"} onClick={() => goLink("velog")}>
+      <ButtonStyle className={"velog"} onClick={() => goLink(buttonName)}>
         velog
       </ButtonStyle>
     );
   } else {
     return (
-      <ButtonStyle className={"notion"} onClick={() => goLink("notion")}>
+      <ButtonStyle className={"notion"} onClick={() => goLink(buttonName)}>
         portfolio - notion
       </ButtonStyle>
     );
