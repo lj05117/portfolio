7539960b046c4be097fb1b1e74382198140cdce3
lj05117 <lj05117@naver.com>
2023-07-18 19:56:00 +0900

    프로젝트 페이지 구성

---
+++ src/pages/ProjectPage/index.tsx
@@ -0,0 +1,13 @@
+import Project from "../../components/Project";
+
+const ProjectPage = () => {
+  return (
+    <>
+      <Project name="fyum"></Project>
+      <Project name="dawool"></Project>
+      <Project name="signbridge"></Project>
+    </>
+  );
+};
+
+export default ProjectPage;
