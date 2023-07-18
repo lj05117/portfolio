33ad5a322828837f35a16818b83d0533824cbc44
lj05117 <lj05117@naver.com>
2023-07-18 09:16:10 +0900

    헤더 컴포넌트 생성

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderDiv, HeaderContentDiv, HeaderContent } from "./styles";
+
+const Header = () => {
+  return (
+    <HeaderDiv>
+      <HeaderContentDiv>
+        <HeaderContent href="/">Home</HeaderContent>
+        <HeaderContent href="/experience">Experience</HeaderContent>
+        <HeaderContent href="/project">Project</HeaderContent>
+        <HeaderContent href="/image">Image</HeaderContent>
+      </HeaderContentDiv>
+    </HeaderDiv>
+  );
+};
+export default Header;
